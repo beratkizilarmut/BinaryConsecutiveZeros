34c51f6220b1c9d1e1ba2b1953b1e1a78e4dbbdd
Berat Kızılarmut <berat.kizilarmut@eecelectronics.local>
2022-08-09 10:36:39 +0300

    overall simplified the function and implemented recursive func

--- main.c
+++ main.c
@@ -1,55 +1,39 @@
 #include <stdio.h>
 
-int binary[32]; /* Creating binary value array for 32 bit integer */
-
-int *Binary_Conversion(int decimal) /* Converting given integer to binary */
+int Bit(int decimal) /* Converting given integer to binary */
 {
-    for(int i=0; decimal > 0; i++)
-    {
-        binary[i] = decimal % 2; /* Storing the remainder from 2 divison */
-        decimal = decimal / 2; /* Halving the number */
-    }
-    return binary; /* Return binary[0] address */
+    return (decimal % 2); /* Return remainer */
 }
 
 void Binary_Consecutive_Zero_Counter(int val) /* Checking for consecutive zeros on a given integer */
 {   
-    int *binary_ptr; /* Pointer to gather binary value from binary converter */
     int prev_bit=0, counter=0, highest_counter=0; /* Creating flags */
-    binary_ptr = Binary_Conversion(val); /* Getting converted binary array */
-    for (int i = 31; i >= 0; i-- ) 
+
+    for(int i = 0; val > 1; i++) /* Loopin up UNTIL zero */
     {
-        printf( "(Parse %d) : %d\n", i, *(binary_ptr + i));
-        if(prev_bit == 0 && counter == 0 && *(binary_ptr + i) == 0)
-        {/* Do nothing, ignore padded zeros on the array */}
-        else
+        if(Bit(val) == 1) /* Bit is one */
         {
-            if(*(binary_ptr+i) == 0) /* Bit is zero */
-            {
-                if(prev_bit == 1) /* Previous bit zero */
-                    counter=1; /* First zero in the chain */
-                else
-                    counter++; /* Not the first zero in the chain */
-                prev_bit=0; /* Set previous bit as zero */
-            }
-            else /* Bit is one */
-            {
-                counter=0; /* Clear current counter */
-                prev_bit=1; /* Set previous bit as one */
-            }
-                
-            if(counter > highest_counter) /* Check if we have highest counter */
-                highest_counter=counter; /* Assign new highest counter */
+            prev_bit=1; /* Set previous bit as one */
+            counter=0; /* Clear current counter */
         }
+        else if(Bit(val) == 0) /* Bit is zero */
+        {
+            prev_bit=0; /* Set previous bit as zero */
+            counter++; /* Increase counter */
+        }
+        if(counter > highest_counter) /* Check if we have highest counter */
+            highest_counter = counter; /* Assign new highest counter */
+        val = val / 2; /* Halved the value */
+        printf("bit number [%d], bit [%d] \n", i, val % 2); /* Printing bit number and bit value */
     }
-    printf("Highest amount of consecutive zeros on the binary is %d", highest_counter);
+    printf("Highest amount of consecutive zeros on the binary is %d", highest_counter); /* Printing result */
 }
 
 int main()
 {   
-    int input;
-    printf("State your input for Binary Consecutive Zero Counter: ");
-    scanf("%d", &input);
-    Binary_Consecutive_Zero_Counter(input);
+    int input; /* Creating input variable */
+    printf("State your input for Binary Consecutive Zero Counter: "); /* Printing input request */
+    scanf("%d", &input); /* Taking input */
+    Binary_Consecutive_Zero_Counter(input); /* Calculating result */
     return 0;
 }
